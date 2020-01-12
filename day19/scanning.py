c9f8610a3598f75fe024568eefbef3372c54f933
rsrsl <riccardosven@gmail.com>
2020-01-12 11:12:05 +0100

    Day 19: refactoring

--- day19/scanning.py
+++ day19/scanning.py
@@ -1,152 +1,93 @@
+"Day 19: Tractor Beam"
+
+from functools import lru_cache
 from intcodecomputer import IntcodeComputer
 
-with open("input") as fhandle:
-    cpu = IntcodeComputer("".join(fhandle.readlines()))
 
-memo = dict()
-def tractor(x, y):
-    if (x, y) in memo:
-        return memo[(x, y)]
-    cpu.reset()
-    cpu.step(x)
-    cpu.step(y)
-    try:
-        val = cpu.step(None)
-    except StopIteration:
-        pass
-    memo[(x, y)] = val
-    return val
+class Area:
+    "Area closest to the ship"
 
-#grid = [[tractor(x, y)  for x in range(50)] for y in range(20)]
+    def __init__(self):
+        with open("input") as fhandle:
+            self.cpu = IntcodeComputer(fhandle.read().strip())
+
+    @lru_cache
+    def tractor(self, pos):
+        "Check if a position is within the tractor beam "
+        self.cpu.reset()
+        self.cpu.step(pos[0])
+        self.cpu.step(pos[1])
+        try:
+            val = self.cpu.step(None)
+        except StopIteration:
+            pass
+        return val
+
+    def countaffected(self, width, height):
+        "Count number of points affected by the tractor beam"
+        naffected = 0
+        for i in range(height):
+            for j in range(width):
+                if self.tractor((i, j)):
+                    naffected += 1
+
+        return naffected
 
 
-class RectZone:
-    def __init__(self, x, y, width, height ):
-        self.x = x
-        self.y = y
+class RectZone(Area):
+    "Rectangular zone to fit Santa's ship"
+
+    def __init__(self, pos, width, height):
+        super().__init__()
+        self.x = pos[0]
+        self.y = pos[1]
         self.width = width
         self.height = height
-    
+
     @property
     def northeast(self):
+        "upper left"
         return (self.x + self.width - 1, self.y)
 
     @property
     def southwest(self):
+        "lower right"
         return (self.x, self.y + self.height - 1)
 
     @property
     def northwest(self):
+        "upper right"
         return (self.x, self.y)
 
     @property
     def southeast(self):
-        return (self.x + self.width - 1, self.y + self.height -1)
-    
+        "lower left"
+        return (self.x + self.width - 1, self.y + self.height - 1)
+
     def adapt(self):
-        while True:
-#            grid[self.y][self.x] = 2
-#            grid[self.y + self.height - 1][self.x] = 2
-#            grid[self.y][self.x + self.width - 1] = 2
-#            grid[self.y + self.height - 1][self.x + self.width - 1] = 2
-#            for row in grid:
-#                for e in row:
-#                    if e == 0:
-#                        print('.', end ="")
-#                    if e == 1:
-#                        print('#', end ="")
-#                    if e == 2:
-#                        print('O', end ="")
-#                print()
-#            input()
-            if not tractor(*self.northeast):
-                self.y += 1
-            if not tractor(*self.southwest):
-                self.x += 1
-            if tractor(*self.northeast) and tractor(*self.northwest) and tractor(*self.southeast) and tractor(*self.southwest):
-                return True
+        "Tune the beam to fit in the tractor area"
+        while not (self.tractor(self.northeast) and self.tractor(self.northwest) and self.tractor(self.southeast) and self.tractor(self.southwest)):
+            # It is not fully inside
+            if not self.tractor(self.northeast):
+                self.y += 1  # We can move it down
+            if not self.tractor(self.southwest):
+                self.x += 1  # We can move it right
 
 
-
-    
-##grid = [['#' if tractor(x, y) else "." for x in range(100)] for y in range(100)]
-#grid = [[tractor(x, y)  for x in range(50)] for y in range(20)]
-
-#rect = RectZone(17, 8, 5, 5)
-#rect.adapt()
-
-#grid[rect.y][rect.x] = "O"
-#grid[rect.y + rect.height - 1][rect.x] = "O"
-#grid[rect.y][rect.x + rect.width - 1] = "O"
-#grid[rect.y + rect.height - 1][rect.x + rect.width - 1] = "O"
-#print("\n".join(["".join(['#' if e else '.' for e in row]) for row in grid]))
+def star1():
+    "Solution to first star"
+    area = Area()
+    print("Star 1:", area.countaffected(50, 50))
 
 
+def star2():
+    "Solution to second star"
+    # Start the area in a known tractor beam position
+    rect = RectZone((17, 8), 100, 100)
+    rect.adapt()
+    print("Star 2:", 10000*rect.x + rect.y)
 
 
-#for row in grid:
-#    print(sum(row))
-
-    
-rect = RectZone(17, 8, 100, 100)
-rect.adapt()
-print(rect.x, rect.y)
-
-
-#
-# def inside(x, y, width, height):
-#    if (x, y) in memo:
-#        return memo[(x, y)
-#    for dx in range(width):
-#        for dy in range(height):
-#                cpu.reset()
-#                cpu.step(x+dx)
-#                cpu.step(y+dy)
-#                try:
-#                    val = cpu.step(None)
-#                except StopIteration:
-#                    val = 0
-#                memo[(x+dx, y+dy)] = val
-#                if not val:
-#                    return 0
-#    return 1
-#
-#print(inside(0, 0, 1, 1))
-#print(inside(5, 3, 1, 1))
-#print(inside(3, 5, 1, 1))
-#print(inside(14, 6, 2, 2))
-#print(inside(6, 14, 2, 2))
-
-#grid = [[0 for _ in range(100)] for _ in range(100)]
-# for i in range(100):
-#    for j in range(100):
-#        cpu.reset()
-#        cpu.step(i)
-#        cpu.step(j)
-#        try:
-#            val = cpu.step(None)
-#            grid[i][j] = val
-#            if inside(i, j, 2, 2):
-#                grid[i][j] = 2
-#            npulled += val
-#        except StopIteration:
-#            pass
-#
-#
-#
-#
-#
-# for row in grid:
-#    for e in row:
-#        if e == 0:
-#            print(".", end="")
-#        if e == 1:
-#            print("#", end="")
-#        if e == 2:
-#            print("O", end="")
-#    print()
-#
-# print("\n".join(["".join("#" if e else "." for e in row) for row in grid]))
-#
-# print("\n".join(["".join("#" if e else "." for e in row) for row in grid]))
-# print(npulled)
+if __name__ == "__main__":
+    star1()
+    star2()
